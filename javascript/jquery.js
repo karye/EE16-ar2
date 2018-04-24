1fe1088421636a75e47114d04b02e5de42adab2e
Karim Ryde <karim.ryde@learnet.se>
2018-04-24 15:07:27 +0200

    Repetera Javascript - Övade på .html(), .css() och .append() - Övning "Hitta presenten", med övning på $(this)

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
+$(function() {
     $("p").hide();
 
-    $("#varning1").click(function(event) {
+    $("#varning1").on('click', function(event) {
         $("p").fadeToggle();
+    });
+
+    // Byt ut all html inuti body med img-taggen
+    $("#nuke").on('click', function(event) {
+        $('body').html('<img src="images/tsar_bomba.jpg">');
+    });
+
+    // Infoga mer text
+    $("#visaMer").on('click', function(event) {
+        $('p').append('<p>Heil Hitler, jag hade nästan lyckats me\'re<br>' +
+        'Heil Hitler, vad jag var stolt när alla skrek det<br>' +
+        'Heil Hitler, jag var en bit på väg mot en värld som skulle frunka utan lyx' + 'och<br> överflöd, men det ända den fick möta var förintelse och död');
+        $('body').css('background', 'url("images/meduza.jpg")').css('color', '#FFF');
     });
 });
---
+++ javascript/presenten.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.ruta').on('click', function() {
+        $(this).css('background', 'url("images/quino-al-638426-unsplash.jpg")');
+    });
+});
